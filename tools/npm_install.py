b385087598d6e5c8c34249062e395217a076a1b0
Ryan Nemiroff <ryguyn@gmail.com>
2019-08-19 23:41:20 -0700

    fix issue with npm_install.py

--- tools/npm_install.py
+++ tools/npm_install.py
@@ -10,7 +10,9 @@ def npm_install():
     if os.path.isfile("package.json"):
         check_deps = str(pathlib.Path("./node_modules/.bin/check-dependencies"))
         if (not os.path.isfile(check_deps)) or (subprocess.call([check_deps], shell=True) != 0):
-            sys.exit(subprocess.call([npm_cmd, "install"]))
+            exit_code = subprocess.call([npm_cmd, "install"])
+            if exit_code != 0:
+                sys.exit(exit_code)
     else:
         print("Error: package.json not present")
         sys.exit(1)
